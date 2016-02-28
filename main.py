611bd7ba1ae70cf57d9480c90c55c390ca08d337
Pete Douma <pete.douma@gmail.com>
2016-02-28 14:22:24 +0000

    move the authorization into the code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime                       import datetime
 from oauth2client.service_account   import ServiceAccountCredentials
 from flask                          import Flask, render_template
 
-app = Flask(__name__, static_url_path='/static')
+application = Flask(__name__, static_url_path='/static')
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -14,13 +14,11 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name( CURRENT_DIR + '/
                                                                  scopes = ['https://spreadsheets.google.com/feeds',
                                                                            'https://www.googleapis.com/auth/drive' ]
                                                               )
-SpreadSheet = gspread.authorize(credentials)
-
-
-@app.route('/')
+@application.route('/')
 def index():
-    calendar = SpreadSheet.open('Coastal Calendar')
-    speakers = calendar.worksheet("Speakers")
+    spreadsheet = gspread.authorize(credentials)
+    calendar    = spreadsheet.open('Coastal Calendar')
+    speakers    = calendar.worksheet("Speakers")
     
     speaker_details = []
     for r, row in enumerate( speakers.get_all_values()):
@@ -51,18 +49,16 @@ def index():
         
     return render_template( 'main.html', **context )
 
-@app.errorhandler(404)
+@application.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, Nothing at this URL.', 404
 
 
-@app.errorhandler(500)
+@application.errorhandler(500)
 def application_error(e):
     """Return a custom 500 error."""
     return 'Sorry, unexpected error: {}'.format(e), 500
 
-"""
 if __name__ == '__main__':     
-    app.run(debug = False,  port=8000)
-"""
+    application.run(debug = False, host='0.0.0.0', port=8000)
